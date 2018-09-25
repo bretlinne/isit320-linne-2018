64be118a49c94e021140363d8a69c7acf81e6a93
Bret at school terminal <bretlinne@gmail.com>
2018-09-24 17:18:29 -0700

    finished react-basics

---
+++ week02-react-basics/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import ReactSimple from './react-simple';
+import { ReactBasics } from './ReactBasicsStatelessFunctional.js';
+
+ReactDOM.render(
+    <ReactBasics/>,
+    document.getElementById("root")
+);
