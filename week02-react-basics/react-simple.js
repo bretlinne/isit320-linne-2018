43ecb81fd968c36b7e0d97968c82226abaac5b0f
Bret at school terminal <bretlinne@gmail.com>
2018-09-26 17:02:12 -0700

    week 2 wednesday

--- week02-react-basics/react-simple.js
+++ week02-react-basics/react-simple.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// exporting a class out of this file.
 export default class ReactBasics extends React.Component {
     render() {
         return <h1>An H1 element in a React Component</h1>;
