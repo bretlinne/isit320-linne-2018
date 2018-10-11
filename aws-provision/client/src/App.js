ebdef81baacf848af35ff422ee664621d030c2c7
Bret at school terminal <bretlinne@gmail.com>
2018-10-10 17:19:15 -0700

    finished Ec2-provision-repo

---
+++ aws-provision/client/src/App.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            file: 'File name will be placed here.',
+            status: 'waiting for server'
+        };
+    }
+
+    queryServer = () => {
+        const that = this;
+        fetch('/foo')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+                that.setState(foo => (json));
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+
+    createEducate = () => {
+        //const that = this;
+        fetch('/create-educate')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+                //that.setState(foo => (json));
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+
+
+                <p className="App-intro">
+                    state: {this.state.status} file: {this.state.file}
+                </p>
+                <button onClick={this.queryServer}>Bar</button>
+                <button onClick={this.createEducate}>Create Educate</button>
+            </div>
+        );
+    }
+}
+
+export default App;
