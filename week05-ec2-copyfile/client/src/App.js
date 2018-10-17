6b1619a7962c5ab30f454304b4c5196faa408f7b
Bret at school terminal <bretlinne@gmail.com>
2018-10-17 16:58:34 -0700

    finished ec2-copy-script

---
+++ week05-ec2-copyfile/client/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+
+    copyScript = () => {
+        fetch('/script-pusher/copy-script')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+  render() {
+    return (
+      <div className="App">
+        <header>
+              <h1>Copy File</h1>
+        </header>
+          <main>
+              <button onClick={this.copyScript}>Copy Script</button>
+          </main>
+          <footer>
+              <p>&copy; by Bret Linne</p>
+          </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
