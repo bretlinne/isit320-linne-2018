10a9c933034854b005f9c9252b8ce5442db490af
Your Name <you@example.com>
2018-10-20 00:11:06 -0700

    finished week05-rest-test

---
+++ week05-rest-test/client/src/App.test.js
@@ -0,0 +1,23 @@
+import { configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import elfDebugEnzyme from './ElfDebugEnzyme';
+
+configure({ adapter: new Adapter() });
+
+describe('rest basic tests', function(){
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+    it('renders initial value of paragraph with state.nine', () =>{
+        const wrapper = shallow(<App/>);
+        const unknown = <p className="App-intro">file: unknown</p>
+        elfDebugEnzyme.getLast(wrapper, 'p', true);
+        expect(wrapper.contains(unknown)).toEqual(true);
+    });
+})
