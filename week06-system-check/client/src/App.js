5d43bb587dd4373e444e9a64852f435bd319d437
Bret at school terminal <bretlinne@gmail.com>
2018-10-24 17:21:28 -0700

    progress on system-check

--- week06-system-check/client/src/App.js
+++ week06-system-check/client/src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            allData: "unknown",
+        };
+    }
+
+    copyScript = () => {
+        const that = this;
+        fetch('/script-pusher/copy-script')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json.allData);
+                that.setState({allData: json.allData});
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>Copy File</h1>
+                </header>
+                <main>
+                    <button onClick={this.copyScript}>Copy Script</button>
+                    <pre>{this.state.allData}</pre>
+                </main>
+                <footer>
+                    <p>&copy; by Bret Linne</p>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
