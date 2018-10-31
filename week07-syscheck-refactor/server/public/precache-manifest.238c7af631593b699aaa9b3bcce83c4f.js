d1d68506618d89706cb4cc3ab23a0662a08c6cd9
Bret at school terminal <bretlinne@gmail.com>
2018-10-31 16:35:22 -0700

    added the build-copy script for midterm

---
+++ week07-syscheck-refactor/server/public/precache-manifest.238c7af631593b699aaa9b3bcce83c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5542d5fe61213ccf6034",
+    "url": "/static/js/main.5542d5fe.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "5542d5fe61213ccf6034",
+    "url": "/static/css/main.6b3c64ae.chunk.css"
+  },
+  {
+    "revision": "5e950f34e967abd1b9ec09902c932d62",
+    "url": "/index.html"
+  }
+];
