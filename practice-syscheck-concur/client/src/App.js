8e84c233cd22d216af3e654ed7acc083081d691c
Your Name <you@example.com>
2018-11-25 22:01:36 -0800

    practice projects added

---
+++ practice-syscheck-concur/client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor (props){
+        super(props);
+        this.state = {
+            allData: 'unknown'
+        };
+    }
+
+    systemCheck = () => {
+        const that = this;
+        fetch('/script-pusher/system-check')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(response) {
+                that.setState({allData: response.allData});
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down; ALL WILL DIE', ex);
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>Practice System Check</h1>
+                </header>
+                <main>
+                    <button onClick={this.systemCheck}>System Check</button>
+                    <pre>{this.state.allData}</pre>
+                </main>
+                <footer>
+                    <p>&copy; by Bret Linne</p>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ practice-syscheck-concur/server/routes/script-pusher.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+const spawn = require('child_process').spawn;
+
+let allData = "";
+
+const systemCheck = () => {
+
+    return new Promise(function (resolve, reject) {
+
+        console.log("Run CpuInfo on LocalSystem", process.env.SETUP_LINUXBOX);
+
+        const pushScript = spawn(process.env.SETUP_LINUXBOX + '/CpuInfo');
+
+        pushScript.stdout.on('data', (data) => {
+            console.log(`child stdout:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.log('PUSH', data);
+        });
+
+        pushScript.stderr.on('data', (data) => {
+            console.log(`child stderr:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.error('PUSH', data);
+        });
+
+        pushScript.on('close', (code) => {
+            resolve({
+                result: 'success',
+                allData: allData,
+                code: code
+            });
+        });
+
+        pushScript.on('error', (code) => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
+
+router.get('/system-check', function(request, response) {
+    'use strict';
+    systemCheck()
+        .then((result) => {
+            console.log(JSON.stringify(result, null, 4));
+            response.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            response.send(err);
+        })
+});
+module.exports = router;
