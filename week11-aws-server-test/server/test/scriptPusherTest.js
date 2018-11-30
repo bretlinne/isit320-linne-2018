34f1a1175c5db07256b863fa571d84c06d83848b
Your Name <you@example.com>
2018-11-30 13:29:24 -0800

    final tweaks on week11-aws-server-test

--- week11-aws-server-test/server/test/scriptPusherTest.js
+++ week11-aws-server-test/server/test/scriptPusherTest.js
@@ -2,10 +2,13 @@ const request = require('supertest');
 const app = require('../app'); //reference to you app.js file
 const assert = require('assert');
 
+let exitCode = 0;
+
 describe('Test script-pusher.js', function() {
 
     //it.only('should call version Check route', function(done) {
     it('should call version Check route', function(done) {
+        this.timeout(4000);
         request(app)
             .get('/script-pusher/run-script?script=VersionCheck')
             .set('Accept', 'application/json')
@@ -13,31 +16,6 @@ describe('Test script-pusher.js', function() {
             //.expect(200, done(), done);
             .expect(200, done);
     });
-
-    // VERSION CHECK TEST
-    it.only('should check script-pusher/run-script Version Check', function(done) {
-        this.timeout(5000);
-        request(app)
-            .get('/script-pusher/run-script?script=VersionCheck')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then((result) => {
-
-                assert.equal(result.body.result, 'success');
-                assert.equal(result.body.code, '0');
-                const present = result.body.allData.includes('PUSH-SCRIPT');
-                assert.ok(present);
-                done();
-
-            });
-    });
-
-
-
-
-
-
 
     it('should call script-pusher/foo route', function(done) {
         request(app)
@@ -60,35 +38,53 @@ describe('Test script-pusher.js', function() {
             });
     });
 
-    //HERE
+    // VERSION CHECK TEST
+    it('should check script-pusher/run-script Version Check', function(done) {
+        this.timeout(5000);
+        request(app)
+            .get('/script-pusher/run-script?script=VersionCheck')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((result) => {
+
+                assert.equal(result.body.result, 'success');
+                assert.equal(result.body.code, exitCode);
+                const present = result.body.allData.includes('PUSH-SCRIPT');
+                assert.ok(present);
+                done();
+
+            });
+    });
 
     // CPU INFO CHECK
     it('should check script-pusher/run-script Cpu Info', function(done) {
+        this.timeout(4000);
         request(app)
             .get('/script-pusher/run-script?script=CpuInfo')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
             .then((result) => {
-                this.timeout(4000);
                 assert.equal(result.body.result, 'success');
-                assert.equal(result.body.code, '1');
+                assert.equal(result.body.code, exitCode);
                 const present = result.body.allData.includes('model name');
                 assert.ok(present);
                 done();
             });
     });
+
     // UPTIME CHECK
     it('should check script-pusher/run-system-tool Uptime', function(done) {
+        this.timeout(4000);
         request(app)
             .get('/script-pusher/run-system-tool?script=Uptime')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
             .then((result) => {
-                this.timeout(4000);
                 assert.equal(result.body.result, 'success');
-                assert.equal(result.body.code, '1');
+                assert.equal(result.body.code, exitCode);
                 const present = result.body.allData.includes('UPTIME:');
                 assert.ok(present);
                 done();
