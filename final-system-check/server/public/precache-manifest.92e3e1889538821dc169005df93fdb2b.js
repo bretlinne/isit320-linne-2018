997c05a29c729a7e3e419c888ef9694c38b5c59f
Your Name <you@example.com>
2018-12-06 18:27:52 -0800

    made final tweaks to final-system-check

---
+++ final-system-check/server/public/precache-manifest.92e3e1889538821dc169005df93fdb2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "26d01c5232ae525d4cd5",
+    "url": "/static/js/main.26d01c52.chunk.js"
+  },
+  {
+    "revision": "0fa7456b079361ec652b",
+    "url": "/static/js/1.0fa7456b.chunk.js"
+  },
+  {
+    "revision": "26d01c5232ae525d4cd5",
+    "url": "/static/css/main.6da4bcc9.chunk.css"
+  },
+  {
+    "revision": "cfd1a30bb9799ce85121103ec5362bdb",
+    "url": "/index.html"
+  }
+];
---
+++ final-system-check/server/routes/script-pusher.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var router = express.Router();
+
+const spawn = require('child_process').spawn;
+const uptimePath = '/usr/bin/uptime';
+const SETUP_LINUXBOX = '/home/bcuser/Git/JsObjects/Utilities/SetupLinuxBox';
+let allData = '';
+
+const runSystemTool = () => {
+    return new Promise(function(resolve, reject) {
+        console.log('Run Uptime on LocalSystem', uptimePath);
+        /**/
+        const invokeCommand = spawn(uptimePath);
+        invokeCommand.stdout.on('data', data => {
+            console.log(`child stdout:\n${data}`);
+            allData += 'UPTIME: ' + data;
+        });
+
+        invokeCommand.stderr.on('data', data => {
+            console.log(`child stderr:\n${data}`);
+            allData += 'UPTIME: ' + data;
+            //console.error('PUSH', data);
+        });
+
+        invokeCommand.on('close', code => {
+            resolve({
+                result: 'success',
+                allData: allData,
+                code: code
+            });
+        });
+
+        invokeCommand.on('error', code => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
+const runScript = script => {
+    return new Promise(function(resolve, reject) {
+
+        console.log('Run CpuInfo on LocalSystem', SETUP_LINUXBOX);
+
+        //const pushScript = spawn(process.env.SETUP_LINUXBOX + '/CpuInfo');
+        const pushScript = spawn(process.env.SETUP_LINUXBOX + '/' + script);
+        //const pushScript = spawn(SETUP_LINUXBOX + '/' + script);
+
+        pushScript.stdout.on('data', data => {
+            console.log(`child stdout:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.log('PUSH', data);
+        });
+
+        pushScript.stderr.on('data', data => {
+            console.log(`child stderr:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.error('PUSH', data);
+        });
+
+        pushScript.on('close', code => {
+            resolve({
+                result: 'success',
+                allData: allData,
+                code: code
+            });
+        });
+
+        pushScript.on('error', code => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
+
+router.get('/run-script', function(request, response) {
+    'use strict';
+    console.log('QUERY: ', request.query.script);
+    allData = '';
+    runScript(request.query.script)
+        .then(result => {
+            console.log(JSON.stringify(result, null, 4));
+            response.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            response.send(err);
+        });
+});
+
+router.get('/run-system-tool', function(request, response) {
+    'use strict';
+    console.log('QUERY: ', request.query.script);
+    allData = '';
+    runSystemTool(request.query.script)
+        .then(result => {
+            console.log(JSON.stringify(result, null, 4));
+            response.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            response.send(err);
+        });
+});
+
+module.exports = router;
