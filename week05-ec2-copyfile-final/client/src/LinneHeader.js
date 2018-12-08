b60542a661e7af7239794baf7114fefc14d65aac
Your Name <you@example.com>
2018-12-07 18:34:04 -0800

    final tweaks to week05-ec2-copy-final; midterm-code; week03-rest-basics-final

---
+++ week05-ec2-copyfile-final/client/src/LinneHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class LinneHeader extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>Final EC2 Copy File</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default LinneHeader;
